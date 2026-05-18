43ba4fd9ca527b3dd30e2ba7c5693f9bc959ef09
Lucas Daniel <lucas.d.teixeira@edu.ufes.br>
2026-05-18 15:50:56 -0300

    deleting initial project from nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <div>
-        <Image src="logo.svg" alt="Logo Work & Rest" width={50} height={50} />
-        <h1 className="">Work & Rest</h1>
-      </div>
+      <h1>Hello, work & rest!</h1>
     </div>
   );
 }
